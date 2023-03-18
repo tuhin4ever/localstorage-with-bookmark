39ec0542357e7ff52438f4c9add445aec1e7f276
tuhin_4ever <tuhin4ever19@gmail.com>
2023-03-18 18:47:14 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const loadProduct = () => {
+  fetch("./product.json")
+    .then((response) => response.json())
+    .then((data) => displayProduct(data));
+};
+
+const displayProduct = (data) => {
+  const cards = document.getElementById("cards");
+  data.forEach((product) => {
+    const card = document.createElement("div");
+    card.classList.add("card", "m-2");
+    
+
+    card.innerHTML = `
+          <div class="bookmark-icon">
+          <i class="fa-solid fa-bookmark"></i>
+          <i class="fa-regular fa-bookmark"></i>
+        
+
+
+        </div>
+        <div class="product-img-container">
+          <img
+            class="product-img"
+            src=${product.image}
+            alt=""
+          />
+        </div>
+        <h3>${product.name}</h3>
+        <p>The Widget 3000 is the latest and greatest in widget</p>
+        <div class="priceAndButtons">
+          <h2 class="text-primary">$${product.price}</h2>
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+          `;
+    cards.appendChild(card);
+  });
+};
+
+// ! handle book mark
+
+
+
+loadProduct();
